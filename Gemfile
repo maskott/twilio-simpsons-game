4f90df7cad4fabd1b0c8434866ddb7cfdfa70eab
Mike Skott <mike@skott.us>
2015-06-18 00:01:00 -0400

    moving requirements to Gemfile

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "https://rubygems.org"
 ruby "2.2.2"
 gem 'sinatra', '1.1.0'
+gem 'twilio-ruby', '~> 4.2.0'
--- twiml-quickstart.rb
+++ twiml-quickstart.rb
@@ -1,7 +1,3 @@
-require 'rubygems'
-require 'sinatra'
-require 'twilio-ruby'
- 
 get '/hello-monkey' do
   Twilio::TwiML::Response.new do |r|
     r.Say 'Hello Monkey'
