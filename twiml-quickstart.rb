c0b4cb25be3dc0ef8d60d9469edbd1e162bf7ffa
Mike Skott <mike@skott.us>
2015-06-18 01:24:26 -0400

    sounds, script rewrite

--- twiml-quickstart.rb
+++ twiml-quickstart.rb
@@ -2,47 +2,47 @@ require 'rubygems'
 require 'sinatra'
 require 'twilio-ruby'
 
-get '/hello-monkey' do
+get '/hello' do
   people = {
-    '+14158675309' => 'Curious George',
-    '+14158675310' => 'Boots',
-    '+14158675311' => 'Virgil',
-    '+14158675312' => 'Marcel',
+    '+14047180928' => 'Corinne',
+    '+14043755575' => 'Mike',
   }
-  name = people[params['From']] || 'Monkey'
+  name = people[params['From']] || 'Caller'
   Twilio::TwiML::Response.new do |r|
-    r.Say "Hello #{name}"
-    r.Play 'http://demo.twilio.com/hellomonkey/monkey.mp3'
-    r.Gather :numDigits => '1', :action => '/hello-monkey/handle-gather', :method => 'get' do |g|
-      g.Say 'To speak to a real monkey, press 1.'
-      g.Say 'Press 2 to record your own monkey howl.'
+    r.Say "Hello #{name}, Welcome to"
+    r.Play 'sounds/beep14.mp3'
+    r.Say "Twilio Test"
+    r.Play 'sounds/beep15.mp3'
+    r.Gather :numDigits => '1', :action => '/hello/handle-gather', :method => 'get' do |g|
+      g.Say 'To call Mikes cell phone, press 1.'
+      g.Say 'Press 2 to record your own annoying voice.'
       g.Say 'Press any other key to start over.'
     end
   end.text
 end
 
 
-get '/hello-monkey/handle-gather' do
-  redirect '/hello-monkey' unless ['1', '2'].include?(params['Digits'])
+get '/hello/handle-gather' do
+  redirect '/hello' unless ['1', '2'].include?(params['Digits'])
   if params['Digits'] == '1'
     response = Twilio::TwiML::Response.new do |r|
       r.Dial '+14043755575'
-      r.Say 'The call failed or the remote party hung up. Goodbye.'
+      r.Say 'Mikes phone is no longer available to you at this time. Goodbye.'
     end
   elsif params['Digits'] == '2'
     response = Twilio::TwiML::Response.new do |r|
-      r.Say 'Record your monkey howl after the tone.'
-      r.Record :maxLength => '30', :action => '/hello-monkey/handle-record', :method => 'get'
+      r.Say 'Record your grating voice after the tone, if you must.'
+      r.Record :maxLength => '30', :action => '/hello/handle-record', :method => 'get'
     end
   end
   response.text
 end
 
 
-get '/hello-monkey/handle-record' do
+get '/hello/handle-record' do
   Twilio::TwiML::Response.new do |r|
-    r.Say 'Listen to your monkey howl.'
+    r.Say 'Seriously, just listen to what you sound like.'
     r.Play params['RecordingUrl']
-    r.Say 'Goodbye.'
+    r.Say 'I can\'t take any more. I just can\t... Goodbye.'
   end.text
 end
