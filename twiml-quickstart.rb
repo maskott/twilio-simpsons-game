5d367a10ed1a8d0986515067e29081ab89f9e9bf
Mike Skott <mike@skott.us>
2015-06-18 00:19:49 -0400

    adding mp3

--- twiml-quickstart.rb
+++ twiml-quickstart.rb
@@ -5,5 +5,6 @@ require 'twilio-ruby'
 get '/hello-monkey' do
   Twilio::TwiML::Response.new do |r|
     r.Say 'Hello Monkey'
+    r.Play 'http://demo.twilio.com/hellomonkey/monkey.mp3'
   end.text
 end
