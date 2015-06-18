3e3ddfe038b86b63dafa1c5bc42131606a62c5f0
Mike Skott <mike@skott.us>
2015-06-18 17:20:30 -0400

    adding new answers, trying out logic for better question separation and abstraction

--- twiml-quickstart.rb
+++ twiml-quickstart.rb
@@ -9,7 +9,21 @@ q1_pool = [
   ["neddoodle", "Ned Flanders", "633"],
   ["exports", "Bart Simpson", "227"],
 ]
-q1 = q1_pool.sample
+q1 =""
+
+q2_pool = [
+  ["surgery", "Doctor Nick Riviera", "642"],
+  ["alcohol", "Mayor Joe Quimby", "563"],
+  ["nelsonhero", "Milhouse Van Houten", "645"],
+]
+q2 = ""
+
+q3_pool = [
+  ["roof", "Cletus Spuckler", "253"],
+  ["game", "Chief Clancy Wiggum", "252"],
+  ["titpecker", "Prinipal Seymore Skinner", "739"],
+]
+q3 = ""
 
 get '/hello' do
   people = {
@@ -37,6 +51,9 @@ end
 get '/hello/simps' do
   redirect '/hello' unless ['1'].include?(params['Digits'])
   points = 0
+  q1 = q1_pool.sample
+  q2 = q2_pool.sample
+  q3 = q3_pool.sample
   Twilio::TwiML::Response.new do |r|
     r.Gather :numDigits => '1', :action => '/hello/simps/1', :method => 'get' do |g|
       g.Say 'To skip these instructions and get straight to the game, press 1 at any time. Press any other number to repeat these instructions.', voice: 'alice'
@@ -49,57 +66,59 @@ get '/hello/simps' do
   end.text
 end
 
+# QUESTION 1
+
 get '/hello/simps/1' do
   redirect '/hello/simps' unless ['1'].include?(params['Digits'])
   Twilio::TwiML::Response.new do |r|
     r.Say 'This first question is worth 10 points.', voice: 'alice'
     r.Play '/sounds/02-coin.mp3'
     r.Say 'Round one... begin!', voice: 'alice'
-    r.Gather :numDigits => '3', :action => '/hello/simps/2', :method => 'get' do |g|
+    r.Gather :numDigits => '3', :action => '/hello/simps/1/a', :method => 'get' do |g|
       r.Play "/sounds/#{q1[0]}.mp3"
       g.Say '... Dial the first three letters of the characters first name.', voice: 'alice'
     end
   end.text
 end
 
-get '/hello/simps/2' do
+get '/hello/simps/1/a' do
   if params['Digits'] == q1[2]
     points += 10
     response = Twilio::TwiML::Response.new do |r|
       r.Play '/sounds/woohoo.mp3'
       r.Say "#{q1[1]} is correct for 10 points!", voice: 'alice'
       r.Say "You now have a total of #{points.to_s} points.", voice: 'alice'
-      r.Play '/sounds/44-coin-2.mp3'
-      r.Say 'This next question is worth 20 points.', voice: 'alice'
-      r.Say 'Round two... begin!', voice: 'alice'
-      r.Gather :numDigits => '3', :action => '/hello/simps/3', :method => 'get' do |g|
-        r.Play '/sounds/surgery.mp3'
-        g.Say '... Dial the first three letters of the characters first name.', voice: 'alice'
-      end
+      r.Redirect "/hello/simps/2"
     end
   else
     response = Twilio::TwiML::Response.new do |r|
       r.Play '/sounds/doh.mp3'
       r.Say "That is incorrect. The correct answer was #{q1[1]} or #{q1[2]}.", voice: 'alice'
       r.Say 'You have no points... May god have mercy upon your soul.', voice: 'alice'
-      r.Play '/sounds/44-coin-2.mp3'
-      r.Say 'This next question is worth 20 points.', voice: 'alice'
-      r.Say 'Round two... begin!', voice: 'alice'
-      r.Gather :numDigits => '3', :action => '/hello/simps/3', :method => 'get' do |g|
-        r.Play '/sounds/surgery.mp3'
-        g.Say '... Dial the first three letters of the characters first name.', voice: 'alice'
-      end
+      r.Redirect "/hello/simps/2"
     end
   end
   response.text
 end
 
+get '/hello/simps/2' do
+  Twilio::TwiML::Response.new do |r|
+    r.Play '/sounds/44-coin-2.mp3'
+    r.Say 'This next question is worth 20 points.', voice: 'alice'
+    r.Say 'Round two... begin!', voice: 'alice'
+    r.Gather :numDigits => '3', :action => '/hello/simps/3', :method => 'get' do |g|
+      r.Play "/sounds/#{q2[0]}.mp3"
+      g.Say '... Dial the first three letters of the characters first name.', voice: 'alice'
+    end
+  end.text
+end
+
 get '/hello/simps/3' do
-  if params['Digits'] == '642'
+  if params['Digits'] == q2[2]
     points += 20
     response = Twilio::TwiML::Response.new do |r|
       r.Play '/sounds/woohoo.mp3'
-      r.Say 'Doctor Nick Riviera is correct for 20 points!', voice: 'alice'
+      r.Say "#{q2[1]} is correct for 20 points!", voice: 'alice'
       r.Say "You now have a total of #{points.to_s} points.", voice: 'alice'
       r.Play '/sounds/45-coin-3.mp3'
       r.Say 'This next question is worth 30 points.', voice: 'alice'
@@ -112,7 +131,7 @@ get '/hello/simps/3' do
   else
     response = Twilio::TwiML::Response.new do |r|
       r.Play '/sounds/doh.mp3'
-      r.Say 'That is incorrect. The correct answer was Doctor Nick Riviera or 6, 4, 2.', voice: 'alice'
+      r.Say "That is incorrect. The correct answer was #{q2[1]} or #{q2[2]}.", voice: 'alice'
       r.Say "You now have a total of #{points.to_s} points.", voice: 'alice'
       r.Play '/sounds/45-coin-3.mp3'
       r.Say 'This next question is worth 30 points.', voice: 'alice'
@@ -127,12 +146,12 @@ get '/hello/simps/3' do
 end
 
 get '/hello/simps/end' do
-  if params['Digits'] == '252'
+  if params['Digits'] == q3[2]
     points += 30
     response = Twilio::TwiML::Response.new do |r|
       r.Play '/sounds/woohoo.mp3'
-      r.Say 'Cheif Clancy Wiggum is correct for 30 points!', voice: 'alice'
-      r.Say "You finished the game with a total of #{points.to_s} points.", voice: 'alice'
+      r.Say "#{q3[1]} is correct for 30 points!", voice: 'alice'
+      r.Say "Congratulations #{name}! You finished the game with a total of #{points.to_s} points.", voice: 'alice'
       r.Play '/sounds/43-game-over.mp3'
       r.Gather :numDigits => '1', :action => '/hello/simps/end-menu', :method => 'get' do |g|
         g.Say 'To play again, press 1 now.', voice: 'alice'
@@ -143,7 +162,7 @@ get '/hello/simps/end' do
   else
     response = Twilio::TwiML::Response.new do |r|
       r.Play '/sounds/doh.mp3'
-      r.Say 'That is incorrect. The correct answer was Cheif Clancy Wiggum or 2, 5, 2.', voice: 'alice'
+      r.Say "That is incorrect. The correct answer was #{q3[1]} or #{q3[2]}.", voice: 'alice'
       r.Say "You finished the game with a total of #{points.to_s} points.", voice: 'alice'
       r.Play '/sounds/43-game-over.mp3'
       r.Gather :numDigits => '1', :action => '/hello/simps/end-menu', :method => 'get' do |g|
