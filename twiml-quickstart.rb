98255c4fe46d6383540e690210535b1d74955112
Mike Skott <mike@skott.us>
2015-06-18 11:33:59 -0400

    figuring out redirection

--- twiml-quickstart.rb
+++ twiml-quickstart.rb
@@ -39,7 +39,17 @@ get '/hello/handle-gather' do
       r.Record :maxLength => '30', :action => '/hello/handle-record', :method => 'get'
     end
   elsif params['Digits'] == '3'
-    redirect '/hello/simps'
+    r.Play '/sounds/simpsons_intro.mp3'
+    r.Say 'Get ready to play the Simpsons audio game!', voice: 'alice'
+    r.Play '/sounds/hacker.mp3'
+    r.Gather :numDigits => '1', :action => '/hello/simps/1', :method => 'get' do |g|
+      g.Say 'To skip these instructions and get straight to the game, press 1 at any time. Press any other number to repeat these instructions.', voice: 'alice'
+      g.Say 'In a moment, you will hear an audio clip from the Simpsons. You must try to determine the name of the character that you hear in the clip.', voice: 'alice'
+      g.Say 'If you hear more than one character, please respond with the first character you hear in the clip.', voice: 'alice'
+      g.Say 'You will enter your response by dialing the first three letters of the characters first name.', voice: 'alice'
+      g.Say 'There will be three audio quotes in each game. Good luck!', voice: 'alice'
+      g.Say 'Press 1 to begin the game, or any other number to repeat these instructions.', voice: 'alice'
+    end
   end
   response.text
 end
@@ -53,22 +63,6 @@ get '/hello/handle-record' do
     r.Say "Twilio Test... out"
     r.Play '/sounds/beep15.mp3'
   end.text
-end
-
-get '/hello/simps' do
-  Twilio::TwiML::Response.new do |r|
-    r.Play '/sounds/simpsons_intro.mp3'
-    r.Say 'Get ready to play the Simpsons audio game!', voice: 'alice'
-    r.Play '/sounds/hacker.mp3'
-    r.Gather :numDigits => '1', :action => '/hello/simps/1', :method => 'get' do |g|
-      g.Say 'To skip these instructions and get straight to the game, press 1 at any time. Press any other number to repeat these instructions.', voice: 'alice'
-      g.Say 'In a moment, you will hear an audio clip from the Simpsons. You must try to determine the name of the character that you hear in the clip.', voice: 'alice'
-      g.Say 'If you hear more than one character, please respond with the first character you hear in the clip.', voice: 'alice'
-      g.Say 'You will enter your response by dialing the first three letters of the characters first name.', voice: 'alice'
-      g.Say 'There will be three audio quotes in each game. Good luck!', voice: 'alice'
-      g.Say 'Press 1 to begin the game, or any other number to repeat these instructions.', voice: 'alice'
-    end
-  end
 end
 
 get '/hello/simps/1' do
